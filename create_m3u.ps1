2f0387a0da87de6fc5a1aa8ac3ff0b929a1bca34
olfolfolf <36164676+olfolfolf@users.noreply.github.com>
2018-05-08 20:19:56 +0200

    Update create_m3u.ps1

--- create_m3u.ps1
+++ create_m3u.ps1
@@ -25,7 +25,7 @@ If (!$InputCSV) {
 (Get-Content $InputCSV).replace(';', ',') | Set-Content $InputCSV
 
 # import and sort CSV
-$Playlist = Import-Csv -Delimiter "," -Path $InputCSV | Sort -Property 'nr'
+$Playlist = Import-Csv -Delimiter "," -Path $InputCSV | Sort-Object -Property 'nr'
 
 # new file / override old file
 $Header = '#EXTM3U'
@@ -36,8 +36,10 @@ ForEach ($Channel in $Playlist){
 	$tvg_name         = $Channel.'tvg-name'
 	$tvg_id           = $Channel.'tvg-id'
 	$group_title      = $Channel.'group-title'
-	$tvg_logo         = [System.Web.HttpUtility]::UrlDecode($Channel.'tvg-logo')
-	$stream           = [System.Web.HttpUtility]::UrlDecode($Channel.url)
+	#$tvg_logo         = [System.Web.HttpUtility]::UrlDecode($Channel.'tvg-logo')
+	#$stream           = [System.Web.HttpUtility]::UrlDecode($Channel.url)
+	$tvg_logo         = $Channel.'tvg-logo'
+	$stream           = $Channel.url
 	$service_name     = $tvg_name.Replace(" ", "\ ")
 	$service_provider = $group_title.Replace(" ", "\ ")
 
@@ -45,7 +47,8 @@ ForEach ($Channel in $Playlist){
 	Write-Host $Line
 	Add-Content -Path $Output -Value $Line
 	
-	$Line = 'pipe:///usr/bin/ffmpeg -loglevel fatal -i {0} -vcodec copy -acodec copy -metadata service_name={1} -metadata service_provider={2} -mpegts_service_type advanced_codec_digital_sdtv -f mpegts pipe:1' -f $stream, $service_name, $service_provider
+	#$Line = 'pipe:///usr/bin/ffmpeg -loglevel fatal -i {0} -vcodec copy -acodec copy -metadata service_name={1} -metadata service_provider={2} -mpegts_service_type advanced_codec_digital_sdtv -f mpegts pipe:1' -f $stream, $service_name, $service_provider
+	$Line = 'pipe:///usr/bin/ffmpeg -loglevel fatal -i {0} -vcodec copy -acodec copy -metadata service_name={1} -metadata service_provider={2} -f mpegts -tune zerolatency pipe:1' -f $stream, $service_name, $service_provider
 	Write-Host $Line
 	Add-Content -Path $Output -Value $Line
 }
